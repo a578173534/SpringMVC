19c7fef9a13aec6ae4b66cbd063ae857173fd087
xtremely <578173534@qq.com>
2020-09-15 12:13:01 +0800

    SpringMVC代码

--- ch04-return/src/main/java/com/ggs/controller/MyController.java
+++ ch04-return/src/main/java/com/ggs/controller/MyController.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ggs.vo.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @RequestMapping: value ： 所有请求地址的公共部分，叫做模块名称
@@ -73,5 +76,78 @@ public class MyController {
         pw.println(json);
         pw.flush();
         pw.close();
+    }
+
+    /**
+     * 处理器方法返回一个Student，通过框架转为json，响应ajax请求
+     *
+     * @ResponseBody: 作用：把处理器方法返回对象转为json后，通过HttpServletResponse输出给浏览器。
+     * 位置：方法的定义上面。 和其它注解没有顺序的关系。
+     * 返回对象框架的处理流程：
+     * 1. 框架会把返回Student类型，调用框架的中ArrayList<HttpMessageConverter>中每个类的canWrite()方法
+     * 检查那个HttpMessageConverter接口的实现类能处理Student类型的数据--MappingJackson2HttpMessageConverter
+     * <p>
+     * 2.框架会调用实现类的write（）， MappingJackson2HttpMessageConverter的write()方法
+     * 把邝贤发同学的student对象转为json， 调用Jackson的ObjectMapper实现转为json
+     * contentType: application/json;charset=utf-8
+     * <p>
+     * 3.框架会调用@ResponseBody把2的结果数据输出到浏览器， ajax请求处理完成
+     */
+    @RequestMapping("/returnStudentJson.do")
+    @ResponseBody
+    public Student doStudentJsonObject(String name, Integer age) {
+
+        //调用service，获取请求结果数据 ， Student对象表示结果数据
+        Student student = new Student("邝贤发同学", 20);
+        return student; // 会被框架转为json
+    }
+
+    /**
+     * 处理器方法返回List<Student>
+     * 返回对象框架的处理流程：
+     * 1. 框架会把返回List<Student>类型，调用框架的中ArrayList<HttpMessageConverter>中每个类的canWrite()方法
+     * 检查那个HttpMessageConverter接口的实现类能处理Student类型的数据--MappingJackson2HttpMessageConverter
+     * <p>
+     * 2.框架会调用实现类的write（）， MappingJackson2HttpMessageConverter的write()方法
+     * 把李四同学的student对象转为json， 调用Jackson的ObjectMapper实现转为json array
+     * contentType: application/json;charset=utf-8
+     * <p>
+     * 3.框架会调用@ResponseBody把2的结果数据输出到浏览器， ajax请求处理完成
+     */
+    @RequestMapping("/returnStudentJsonArray.do")
+    @ResponseBody
+    public List<Student> doStudentJsonObjectArray(String name, Integer age) {
+        List<Student> list = new ArrayList<>();
+
+        //调用service，获取请求结果数据 ， Student对象表示结果数据
+        Student student = new Student("邝贤发同学", 20);
+        list.add(student);
+
+        student = new Student("张三", 28);
+        list.add(student);
+
+        return list;
+    }
+
+    /**
+     * 处理器方法返回的是String ， String表示数据的，不是视图。
+     * 区分返回值String是数据，还是视图，看有没有@ResponseBody注解
+     * 如果有@ResponseBody注解，返回String就是数据，反之就是视图
+     * <p>
+     * 默认使用“text/plain;charset=ISO-8859-1”作为contentType,导致中文有乱码，
+     * 解决方案：给RequestMapping增加一个属性 produces, 使用这个属性指定新的contentType.
+     * 返回对象框架的处理流程：
+     * 1. 框架会把返回String类型，调用框架的中ArrayList<HttpMessageConverter>中每个类的canWrite()方法
+     * 检查那个HttpMessageConverter接口的实现类能处理String类型的数据--StringHttpMessageConverter
+     * <p>
+     * 2.框架会调用实现类的write（）， StringHttpMessageConverter的write()方法
+     * 把字符按照指定的编码处理 text/plain;charset=ISO-8859-1
+     * <p>
+     * 3.框架会调用@ResponseBody把2的结果数据输出到浏览器， ajax请求处理完成
+     */
+    @RequestMapping(value = "/returnStringData.do", produces = "text/plain;charset=utf-8")
+    @ResponseBody
+    public String doStringData(String name, Integer age) {
+        return "Hello SpringMVC 返回对象，表示数据";
     }
 }
--- ch04-return/src/main/webapp/index.jsp
+++ ch04-return/src/main/webapp/index.jsp
@@ -17,17 +17,28 @@
             $("button").click(function () {
                 //alert("button click");
                 $.ajax({
-                    url: "returnVoid-ajax.do",
+                    //url: "returnVoid-ajax.do",
+                    //url: "returnStudentJsonArray.do",
+                    url: "returnStringData.do",
                     data: {
                         name: "邝贤发",
                         age: 20
                     },
                     type: "post",
-                    dataType: "json",
+                    dataType: "text",
                     success: function (resp) {
+
                         //resp从服务器端返回的是json格式的字符串 {"name":"邝贤发","age":20}
                         //jquery会把字符串转为json对象， 赋值给resp形参。
-                        alert(resp.name + "    " + resp.age);
+
+                        // [{"name":"邝贤发同学","age":20},{"name":"张三","age":28}]
+                        //alert(resp.name + "    " + resp.age);
+
+                        /*$.each(resp, function (i, n) {
+                            alert(n.name + "    " + n.age)
+                        })*/
+
+                        alert("返回的是文本数据：" + resp);
                     }
                 })
             })
